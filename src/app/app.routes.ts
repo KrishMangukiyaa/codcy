801c0c3d16b61214945b62772f274cbfe3d071c3
Krish mangukiya <139554335+KrishMangukiyaa@users.noreply.github.com>
2025-02-21 19:40:17 +0530

    repo update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,53 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DashbordComponent } from './pages/admin/dashbord/dashbord.component';
+import { WriteABlogComponent } from './pages/admin/write-a-blog/write-a-blog.component';
+import { ProfileComponent } from './pages/admin/profile/profile.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { CatagoryComponent } from './pages/catagory/catagory.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'admin',
+        component: DashbordComponent
+    },
+    {
+        path: 'admin/add-post',
+        component: WriteABlogComponent
+    },
+    {
+        path: 'admin/edit-post/:id',
+        component: WriteABlogComponent
+    },
+    {
+        path: 'admin/profile',
+        component: ProfileComponent
+    },
+    {
+        path: 'blog/:id',
+        component: BlogComponent
+    },
+    {
+        path: 'category/:id',
+        component: CatagoryComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: '**',
+        component: PagenotfoundComponent
+    }
 
-export const routes: Routes = [];
+];
